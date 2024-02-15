4fa0c82cf91b13f0ec1a07808a6bd4ea2c3b29f1
MahmadaminLodhiya <mahmadamin.lodhiya@tssconsultancy.com>
2024-02-15 09:37:10 +0530

    add swagger dependency

--- src/main/java/com/BookStoreManagement/BookStore/Controller/BookController.java
+++ src/main/java/com/BookStoreManagement/BookStore/Controller/BookController.java
@@ -1,27 +1,4 @@
 package com.BookStoreManagement.BookStore.Controller;
 
-import com.BookStoreManagement.BookStore.Dto.ServicesResponse;
-import com.BookStoreManagement.BookStore.Entity.Book;
-import com.BookStoreManagement.BookStore.Service.BookService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/v1")
 public class BookController {
-
-    private final BookService _bookService;
-
-    public BookController(BookService bookService){
-        _bookService = bookService;
-    }
-
-    @GetMapping("/GetAllBook")
-    public ResponseEntity<ServicesResponse<List<Book>>> getAllBook(){
-        return  _bookService.getAllBook();
-    }
 }
